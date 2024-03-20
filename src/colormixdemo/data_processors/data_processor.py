e8dfb2ace02ee1f04bad2ca37a5c49519751f8e0
Brenden Pelkie <bgpelkie@gmail.com>
2024-03-19 17:04:09 -0700

    start outlining structure

---
+++ src/colormixdemo/data_processors/data_processor.py
@@ -0,0 +1,10 @@
+# Base class for data processing
+
+
+class data_processor():
+    def __init__(self):
+        pass
+
+
+    def process_results(self):
+        raise NotImplementedError
---
+++ src/colormixdemo/data_processors/image_data_processor.py
@@ -0,0 +1,23 @@
+
+# class to handle image data processing 
+from data_processor import data_processor
+
+class image_data_processor(data_processor):
+    def __init__(self, mask_radius, well_offset):
+        pass
+
+    def process_image(image):
+        """
+        Handle image processing for an image result from color mixing 
+        """
+        pass
+
+        #return RGB
+    
+
+    def compute_loss(target_color, measured_color, loss_function):
+        """
+        Computes loss metric between target and measured
+        """
+
+        # return loss (scalar valued score)
